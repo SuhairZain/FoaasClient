4de9cfbf19e58bb1adbbe91fe40f39147d2a9146
SuhairZain <suhairzain11@gmail.com>
2016-05-01 18:26:36 +0530

    moved endpoint to seperate module

---
+++ components/Endpoint.js
@@ -0,0 +1,8 @@
+/**
+ * Created by SuhairZain on 1/5/16.
+ * Licensed under the MIT License. Read LICENSE prior to usage.
+ */
+
+const FOAAS_BASE_URL = "http://www.foaas.com/";
+
+export default FOAAS_BASE_URL;
